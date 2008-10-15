29ba8c809150c106949533d6d222bf2296244bfd
Henri <henux@users.sourceforge.net>
2008-10-15 07:30:58 +0300

    Added GPL headers to each file.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,17 @@
-# Copyright (C) 2008 Henri Hakkinen
+# Copyright (c) 2008 Henri Hakkinen
 #
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 Provides commands for displaying UNIX manual pages from the Ubuntu Manpage
@@ -11,7 +23,7 @@ import supybot.world as world
 
 # Use this for the version of this plugin.  You may wish to put a CVS keyword
 # in here if you're keeping the plugin in CVS or some similar system.
-__version__ = "1.0.1"
+__version__ = "1.1"
 
 # XXX Replace this with an appropriate author or supybot.Author instance.
 __author__ = supybot.Author('Henri Hakkinen', 'henux',
